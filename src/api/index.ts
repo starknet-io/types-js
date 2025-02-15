175e8aba665688a26feb8e4042ebd395c6395a55
Toni Tabak <tabaktoni@gmail.com>
2025-02-15 13:29:00 +0100

    fix: export all from executable

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,5 +4,6 @@ export * from './errors.js';
 export * from './components.js';
 export * from './nonspec.js';
 export * from './constants.js';
+export * from './executable.js';
 
 export * from './expansions/transactionReceipt.js';
