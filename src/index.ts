944a9f5f2c0eac82b8ec2da2ede72d9b72be5467
Toni Tabak <tabaktoni@gmail.com>
2025-05-13 04:58:54 -0600

    fix: add snip-29 implementation (#37)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * as API from './api/index.js';
 
 export * from './wallet-api/index.js';
 export * as WALLET_API from './wallet-api/index.js';
+
+export * as PAYMASTER_API from './snip-29/index.js';
---
+++ src/snip-29/index.ts
@@ -0,0 +1,4 @@
+export * from './methods.js';
+export * from './errors.js';
+export * from './components.js';
+export * from './nonspec.js';
