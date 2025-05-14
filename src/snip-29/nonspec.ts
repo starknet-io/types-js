93d148d104c2b6c4eec9f977dc141d4743a43170
0xlny <lenny@419labs.io>
2025-05-14 13:45:12 +0200

    :rotating_light: Fix lint

--- src/snip-29/nonspec.ts
+++ src/snip-29/nonspec.ts
@@ -1,13 +1,13 @@
 /**
  * Types that are not in spec but required for UX
  */
+import { TXN_HASH } from '../api';
+import { OutsideExecutionTypedData } from '../wallet-api';
 import {
   ACCOUNT_DEPLOYMENT_DATA,
   EXECUTION_PARAMETERS,
   FEE_ESTIMATE,
-  OUTSIDE_EXECUTION_TYPED_DATA,
   TRACKING_ID,
-  TRANSACTION_HASH,
 } from './components';
 
 // METHOD RESPONSES
@@ -20,14 +20,14 @@ export type BuildDeployTransactionResponse = {
 };
 export type BuildInvokeTransactionResponse = {
   type: 'invoke';
-  typed_data: OUTSIDE_EXECUTION_TYPED_DATA;
+  typed_data: OutsideExecutionTypedData;
   parameters: EXECUTION_PARAMETERS;
   fee: FEE_ESTIMATE;
 };
 export type BuildDeployAndInvokeTransactionResponse = {
   type: 'deploy_and_invoke';
   deployment: ACCOUNT_DEPLOYMENT_DATA;
-  typed_data: OUTSIDE_EXECUTION_TYPED_DATA;
+  typed_data: OutsideExecutionTypedData;
   parameters: EXECUTION_PARAMETERS;
   fee: FEE_ESTIMATE;
 };
@@ -39,8 +39,7 @@ export type BuildTransactionResponse =
 // response paymaster_execute
 export type ExecuteResponse = {
   tracking_id: TRACKING_ID;
-  transaction_hash: TRANSACTION_HASH;
+  transaction_hash: TXN_HASH;
 };
 
 export type AccountDeploymentData = ACCOUNT_DEPLOYMENT_DATA;
-export type OutsideExecutionTypedData = OUTSIDE_EXECUTION_TYPED_DATA;
